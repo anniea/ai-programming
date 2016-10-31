85070d16da1f06c4800cb9d85038e182f4a676b4
mikaelbj <mikaelbj@stud.ntnu.no>
2016-10-31 17:29:42 +0100

    Fix unscaled error, and add solution plotting

--- Project3/helpers.py
+++ Project3/helpers.py
@@ -24,7 +24,7 @@ def read_tsp_data(filename):
 	data_points = np.array(data_points)
 	
 	# scale coordinates to real numbers between 0 and 1
-	data_points /= np.amax(data_points)
+	# data_points /= np.amax(data_points)
 	
 	# close file
 	file.close()
@@ -39,7 +39,7 @@ def read_tsp_data(filename):
 	return data_points/np.amax(data_points), data_points
 
 
-def plot_tsp_data(neuron_points, city_points, num):
+def plot_intermediate_tsp(neuron_points, city_points, num):
 	plt.figure(figsize=(20, 10))
 	# use append to create circular plot
 	# plot tsp cities positions
@@ -47,13 +47,28 @@ def plot_tsp_data(neuron_points, city_points, num):
 			 np.append(city_points[:, 1], city_points[0, 1]),
 			 'ro', label='City Coordinates')
 	
-	# plot neuron positions
+	# plot neuron positions and connections
 	plt.plot(np.append(neuron_points[:, 0], neuron_points[0, 0]),
 			 np.append(neuron_points[:, 1], neuron_points[0, 1]),
-			 'bo-', label='Neuron Coordinates')
+			 'bo-', label='Neuron Scaffold')
 	
 	plt.legend(loc='upper right')
 	plt.title('Self Organizing Map #' + str(num))
 	plt.savefig('intermediate_plots/trial_%d_#%d.png' % (len(listdir('solutions')), num))
+	# plt.show()
+	plt.clf()
+
+
+def plot_solution_tsp(solution_points):
+	plt.figure(figsize=(20, 10))
+	# use append to create circular plot
+	# plot tsp solution
+	plt.plot(np.append(solution_points[:, 0], solution_points[0, 0]),
+			 np.append(solution_points[:, 1], solution_points[0, 1]),
+			 'bo-', label='Solution Traversal')
+	
+	plt.legend(loc='upper right')
+	plt.title('Self Organizing Map Solution')
+	plt.savefig('solutions/trial_%d_solution.png' % len(listdir('solutions')))
 	# plt.show()
 	plt.clf()
