Fix solution calculation error, add solution plotting, and test decay

Project3/self_organizing_map.py
@@ -1,11 +1,11 @@
-from helpers import read_tsp_data, plot_intermediate_tsp
+from helpers import read_tsp_data, plot_intermediate_tsp, plot_solution_tsp
 import numpy as np
-import math
+import math  # TODO: maybe replace all math-functions with numpy-functions?
 
 # global variables
 tsp = 'wi29'
 learning_rate = 1
-iteration_limit = 1000
+iteration_limit = 2500
 no_of_neurons = None
 plotting = True
 
@@ -15,32 +15,35 @@ def main():
 
 	# read data from TSP data set
 	cities_scaled, cities = read_tsp_data(tsp)
-
+	
 	# set parameters based on the data
 	no_of_neurons = len(cities) * 2
-	neighborhood_radius = int(no_of_neurons/10)
+	# neighborhood_radius = int(no_of_neurons/10)
+	neighborhood_radius = int(no_of_neurons/5)  # test of different value
 
 	# create an initial random self-organizing-map (SOM)
 	initial_som = np.random.rand(no_of_neurons, 2)
 	# use the SOM to create a scaffold
-	scaffold = create_scaffold(cities_scaled, initial_som, neighborhood_radius)
+	scaffold = create_scaffold(initial_som, cities_scaled, neighborhood_radius)
 
 	# read solution from scaffold
-	solution = read_solution(cities, scaffold)
-	#TODO: plot solution
+	solution = read_solution(scaffold, cities_scaled)
+	plot_solution_tsp(np.array([cities[i] for i in solution]))
 
-	total_distance = get_total_distance(cities, solution)
-	print('Total distance:', total_distance)
-	print('Initial distance:', get_total_distance(cities, [x for x in range(len(cities))]))
+	print('Solution:', solution)
+
+	print('\nInitial distance:', get_total_distance(cities, [x for x in range(len(cities))]))
+	print('Computed distance:', get_total_distance(cities, solution))
 
 
-def create_scaffold(cities, som, radius):
+def create_scaffold(som, cities_scaled, radius):
+	global learning_rate
 
 	if plotting:
-		plot_intermediate_tsp(som, cities, 0)
+		plot_intermediate_tsp(som, cities_scaled, 0)
 
 	for i in range(iteration_limit):
-		city = cities[np.random.randint(0, len(cities))]
+		city = cities_scaled[np.random.randint(0, len(cities_scaled))]
 
 		# find the best matching unit (BMU)
 		bmu = get_bmu(som, city)
@@ -49,7 +52,7 @@ def create_scaffold(cities, som, radius):
 		som[bmu] += learning_rate * (city - som[bmu])
 
 		# update neighbors of the bmu according to the neighborhood radius
-		for n in range(1, radius+1):
+		for n in range(1, math.ceil(radius+1)):
 			next_neighbor = bmu + n
 			prev_neighbor = bmu - n
 
@@ -58,26 +61,34 @@ def create_scaffold(cities, som, radius):
 			elif prev_neighbor < 0:
 				prev_neighbor %= no_of_neurons
 
-			som[next_neighbor] += (1 - n/(radius+1)) * learning_rate * (city - som[next_neighbor])
-			som[prev_neighbor] += (1 - n/(radius+1)) * learning_rate * (city - som[prev_neighbor])
+			spatial_decay = 1 - n/math.ceil(radius+1)
+			som[next_neighbor] += spatial_decay * learning_rate * (city - som[next_neighbor])
+			som[prev_neighbor] += spatial_decay * learning_rate * (city - som[prev_neighbor])
 
 		if plotting and not (i+1) % 100:
-			plot_intermediate_tsp(som, cities, i+1)
+			plot_intermediate_tsp(som, cities_scaled, i+1)
 
 		# decrease learning_rate linearly
 		# decrease neighborhood linearly
+		# test reduction
+		if not (i+1) % 10:
+			learning_rate *= 0.99
+			radius *= 0.99
+			# print('Radius as float:', radius, 'and as int', math.ceil(radius))
 
 	return som
 
 
-def read_solution(cities, scaffold):
-	temp = [[] for _ in range(no_of_neurons)]
-	for i in range(len(cities)):
-		temp[get_bmu(scaffold, cities[i])].append(i)
+def read_solution(scaffold, cities_scaled):
+	scaffold_traversal = [[] for _ in range(no_of_neurons)]
+	for i in range(len(cities_scaled)):
+		# add city index to match list of nearest neuron
+		scaffold_traversal[get_bmu(scaffold, cities_scaled[i])].append(i)
 
+	# create solution by adding cities in consecutive order of appearance in scaffold_traversal
 	solution = []
-	for t in temp:
-		for city in t:
+	for neuron_matches in scaffold_traversal:
+		for city in neuron_matches:
 			solution.append(city)
 	return solution
 